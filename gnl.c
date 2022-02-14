ab5b53a40dd709595564b2707561e7f3b2aad9d2
Alexandre BONNISSEAU <abonniss@e1r5p23.42.fr>
2022-02-14 16:12:17 +0100

    pulling the libft repo

--- gnl.c
+++ gnl.c
@@ -1,97 +1,38 @@
 #include "gnl.h"
 
-int search_for_newline(char *str)
+char	*ft_strjoinfree(char **str, char *str_to_join)
 {
-    size_t i;
+	char	*tmp = *str;
 
-    i = 0;
-    while (str != NULL && str[i] != '\0')
-    {
-        if (str[i] == NEW_LINE)
-            return (1);
-        ++i;
-    }
-    return (0); 
+	if (tmp == NULL)
+		*str = ft_strdup(str_to_join);
+	else
+		*str = ft_strjoin(tmp, str_to_join);
+	free(tmp);
+	return *str;
 }
-
-char	*ft_strncpy2(char *dst, const char *src, size_t len)
-{
-	size_t	i;
-
-	i = 0;
-	while (src[i] != '\0' && i < len)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
-char *ft_strconcat(char *str1, char *str2)
-{
-    char *new_str;
-    size_t len_str1;
-    size_t len_str2;
-
-    len_str1 = 0;
-    if (str1 != NULL)
-        len_str1 = ft_strclen(str1, '\0');
-    len_str2 = ft_strclen(str2, '\0');
-    new_str = (char *)malloc(sizeof(char) * (len_str1 + len_str2 + 1));
-    if (new_str == NULL)
-        return (NULL);
-    if (str1 != NULL)
-        ft_strcpy(new_str, str1);
-    ft_strcpy(new_str + len_str1, str2);
-    return (new_str);
-}
-
-char *clean_rest_line(char *str)
-{
-    char *new_str;
-    size_t  i;
-    size_t  j;
-
-    i = 0;
-    while (str[i] != NEW_LINE)
-        ++i;
-    j = i; 
-    while (str[j] != '\0')
-        ++j;
-    new_str = (char *)malloc(sizeof(char) * (j - i) + 1);
-    if (new_str == NULL)
-        return (NULL);
-    j = 0;
-    while (str[i] != '\0')
-    {
-        new_str[j] = str[i];
-        ++j;
-        ++i;
-    }
-    new_str[j] = '\0';
-    free(str);
-    return (new_str); 
-}
-
 
 int read_into_rest(char **rest_line, char **line)
 {
-    
-    if (*rest_line != NULL && search_for_newline(*rest_line) == 1)
-    {
-        if (*line != NULL)
-            free(*line);
-        *line = malloc(sizeof(char) * ft_strclen(*rest_line, NEW_LINE) + 1);
-        if (*line == NULL)
-            return (-1);
-        *line = ft_strncpy2(*line, *rest_line, ft_strclen(*rest_line, NEW_LINE));
-        *rest_line = clean_rest_line(*rest_line);
-        if (*rest_line == NULL)
-            return (-1);
-        return (1);
-    }
-    return (0);
+	char	*new_line;
+	size_t	index_newline;
+	
+	if (*rest_line == NULL)
+		return RET_READ_UNDEFINED;
+	new_line = ft_strchr(*rest_line, '\n');
+	if (new_line == NULL)
+	{
+		*line = ft_strdup(*rest_line);
+		free(*rest_line);
+		*rest_line = NULL;
+		return RET_READ_UNDEFINED;
+	}
+	index_newline = new_line - *rest_line;
+	*line = strndup(*rest_line, index_newline);
+	new_line = ft_strdup(new_line + 1);
+	free(*rest_line);
+	*rest_line = new_line;
+	return LINE_READ;
 }
 
 int assign_line_to_rest(char **line, char **rest_line)
@@ -108,34 +49,38 @@ int read_til_newline(int fd, char **line, char **rest_line)
 {
     char    str_read[BUFF + 1];
     int     ret_read;
+	char	*new_line;
+	size_t	index_newline;
 
     ret_read = read(fd, &str_read, BUFF);
-    str_read[ret_read] = '\0';
-    while (ret_read > 0)
-    { 
-        *rest_line = ft_strconcat(*rest_line, str_read);
-        if (rest_line == NULL)
-            return (-1);
-        if (search_for_newline(*rest_line) == 1)
-            return (read_into_rest(rest_line, line));
-        ret_read = read(fd, &str_read, BUFF);
-        str_read[ret_read] = '\0';
-    }
-    if (ret_read == 0)
-        return (assign_line_to_rest(line, rest_line));
-    return (ret_read); 
+	if (ret_read > 0)
+	{
+    	str_read[ret_read] = '\0';
+		new_line = ft_strchr(str_read, '\n');
+		if (new_line == NULL)                       // ft_strchr retourne NULL s'il ne trouve pas le char dans la *str envoyé
+		{
+			*line = ft_strjoinfree(line, str_read);
+			return (read_til_newline(fd, line, rest_line));
+		}
+		index_newline = new_line - str_read;
+		*rest_line = ft_strjoinfree(rest_line, new_line + 1);
+		str_read[index_newline] = '\0';
+		*line = ft_strjoinfree(line, str_read);
+		return LINE_READ;
+	}
+	return ret_read;
 }
 
 int get_next_line(int fd, char **line)
 {
-    static char *rest_line;
+    static char *rest_line = NULL;
+	int			ret;
 
-    rest_line = NULL;
     if (fd < 0)
-        return (-1);
-    if (read_into_rest(&rest_line, line) == 1)
-        return (1);
-    if (read_into_rest(&rest_line, line) == -1)
-        return (-1);
-    return (read_til_newline(fd, line, &rest_line));
+        return (FAILURE);
+	*line = NULL;
+    ret = read_into_rest(&rest_line, line);
+    if (ret == RET_READ_UNDEFINED)
+    	ret = read_til_newline(fd, line, &rest_line);
+	return ret;
 }
